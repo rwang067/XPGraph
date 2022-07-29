7c5ea51c6e6464a7deef02f259adabd7b83e4d74
ZoRax-A5 <707120797@qq.com>
2022-07-29 16:31:24 +0800

    add expected completion time

--- script/run.sh
+++ script/run.sh
@@ -17,16 +17,19 @@ mkdir results/fig15
 # Figure 11 + Figure 14 + Figure 15
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test GraphOne-P performance, including Figure 11 (Graph ingest time cost for non-volatile systems), Figure 14 (Graph query performance)..." >> script/progress.txt
+echo "[ Expected completion time: around 17.2 hours ]" >> script/progress.txt
 bash script/GraphOne-P.sh
 sleep 10s
 
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test XPGraph performance, including Figure 11 (Graph ingest time cost for non-volatile systems), Figure 14 (Graph query performance), as well as Figure 15 (Graph recovery performance)..." >> script/progress.txt
+echo "[ Expected completion time: around 12.9 hours ]" >> script/progress.txt
 bash script/XPGraph.sh
 sleep 10s
 
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test XPGraph-B performance, including Figure 11 (Graph ingest time cost for non-volatile systems)..." >> script/progress.txt
+echo "[ Expected completion time: around 5.5 hours ]" >> script/progress.txt
 bash script/XPGraph-B.sh
 sleep 10s
 
@@ -34,10 +37,13 @@ sleep 10s
 # Figure 12
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test GraphOne-D performance in DRAM-Only system, including Figure 12 (Graph ingest time cost for volatile systems)..." >> script/progress.txt
+echo "[ Expected completion time: around 1.2 hours ]" >> script/progress.txt
 bash script/GraphOne-D-DO.sh
 sleep 10s
+
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test XPGraph-D performance in DRAM-Only system, including Figure 12 (Graph ingest time cost for volatile systems)..." >> script/progress.txt
+echo "[ Expected completion time: around 0.9 hours ]" >> script/progress.txt
 bash script/XPGraph-D-DO.sh
 # 
 # !! Switch Optane to Memory Mode !! 
@@ -45,14 +51,18 @@ bash script/XPGraph-D-DO.sh
 # 
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test GraphOne-D performance under Memory Mode of Optane, including Figure 12 (Graph ingest time cost for volatile systems)..." >> script/progress.txt
+echo "[ Expected completion time: around 6.6 hours ]" >> script/progress.txt
 bash script/GraphOne-D-MM.sh
 sleep 10s
+
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test XPGraph-D performance under Memory Mode of Optane, including Figure 12 (Graph ingest time cost for volatile systems)..." >> script/progress.txt
+echo "[ Expected completion time: around 4.0 hours ]" >> script/progress.txt
 bash script/XPGraph-D-MM.sh
 
 # Figure 15
 cur_time=$(date "+%Y-%m-%d %H:%M:%S")
 echo $cur_time "Test GraphOne-D performance, including Figure 15 (Graph recovery performance)..." >> script/progress.txt
+echo "[ Expected completion time: around 1.6 hours ]" >> script/progress.txt
 bash script/GraphOne-D-RE.sh
 sleep 10s
